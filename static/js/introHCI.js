24ad8719d537df89487bf0bc6fe07568b9fa53c7
Richard Ying <rying@ucsd.edu>
2015-01-22 11:18:07 -0800

    lab 3 updates

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,47 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       $(".project-description").toggle();
+    }
+}
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+      $("#testjs").text("Please wait...");
+      $(".jumbotron p").toggleClass("active");
 	});
 
+   $("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+   $("#submitBtn").click(updateProject); 
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
